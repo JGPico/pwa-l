e7e8e72b99abc0403212cafc7e172be5a148f0fc
JGPico <minq_2@hotmail.com>
2025-02-21 14:01:55 -0700

    Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
+import shuffle from './utilities/shuffle'
 import './App.css'
+import Card from './components/Card';
 
 function App() {
-  const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState(shuffle);
 
   return (
     <>
       <div className="grid">
+        {cards.map((card) => {
+          const { image, id, matched } = card;
 
+          return (
+            <Card
+              key={id}
+              image={image}
+              selected={false}
+              onClick={() => { }} />
+          );
+        })}
       </div>
-
-      <a href="https://www.freepik.com/search">Icon by Studio MRPOR</a>
 
     </>
 
--- src/utilities/shuffle.js
+++ src/utilities/shuffle.js
@@ -1,7 +1,14 @@
 // combine and shuffle two arrays
 const shuffle = () => {
     const assets = [
-        { image: '/assets/css.png' }
+        { image: '/assets/duck.png' },
+        { image: '/assets/lemon.png' },
+        { image: '/assets/knight.png' },
+        { image: '/assets/bear.png' },
+        { image: '/assets/rum.png' },
+        { image: '/assets/fencing.png' },
+        { image: '/assets/squirrel.png' },
+        { image: '/assets/carrot.png' }
     ];
     return [...assets, ...assets]
         .sort(() => Math.random() - 0.5)
