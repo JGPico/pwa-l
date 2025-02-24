d94553ddb6a97a0c6d807fceff7c1ee8ba21617d
JGPico <minq_2@hotmail.com>
2025-02-24 16:07:39 -0700

    game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,83 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import shuffle from './utilities/shuffle'
 import './App.css'
 import Card from './components/Card';
 
 function App() {
   const [cards, setCards] = useState(shuffle);
+  const [pickOne, setPickOne] = useState(null); // First Selection
+  const [pickTwo, setPickTwo] = useState(null); // Second Selection
+  const [disabled, setDisabled] = useState(false); // Delay handler
+  const [wins, setWins] = useState(0); // Win streak
+
+  // Handle card selection
+  const handleClick = (card) => {
+    if (!disabled) {
+      pickOne ? setPickTwo(card) : setPickOne(card);
+    }
+  };
+
+  const handleTurn = () => {
+    setPickOne(null);
+    setPickTwo(null);
+    setDisabled(false);
+  }
+
+  // Start over
+  const handleNewGame = () => {
+    setWins(0);
+    handleTurn();
+    setCards(shuffle);
+  }
+
+  // Used for selection and match handling
+  useEffect(() => {
+    let pickTimer;
+
+    // Two cards have been clicked
+    if (pickOne && pickTwo) {
+      // check if cards match
+      if (pickOne.image === pickTwo.image) {
+        setCards((prevCards) => {
+          return prevCards.map((cards) => {
+            if (card.image === pickOne.image) {
+              // Update card property to reflect match
+              return { ...card, matched: true };
+            } else {
+              // No match
+              return card;
+            }
+          });
+        });
+        handleTurn();
+      } else {
+        // Prevent new selections until after delay
+        setDisabled(true);
+        // Add delay between selections
+        pickTimer = setTimeout(() => {
+          handleTurn();
+        }, 1000);
+      }
+    }
+
+    return () => {
+      clearTimeout(pickTimer);
+    };
+  }, [cards, pickOne, pickTwo])
+
+  // User finds all matches, handle accordingly
+  useEffect(() => {
+    // Check for remaining card matches
+    const checkWin = cards.filter((card) => !card.matched);
+
+    //All matches made, handle win/badge counter
+    if (cards.length && checkWin.length < 1) {
+      console.log("You win!");
+      setWins(wins + 1);
+      handleTurn();
+      setCards(shuffle);
+    }
+  }, [cards, wins])
 
   return (
     <>
@@ -16,8 +89,8 @@ function App() {
             <Card
               key={id}
               image={image}
-              selected={false}
-              onClick={() => { }} />
+              selected={card === pickOne || card === pickTwo || matched}
+              onClick={() => { handleClick(card) }} />
           );
         })}
       </div>
